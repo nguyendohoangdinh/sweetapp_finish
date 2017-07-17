d2312473d99c64a8cc58cd00848de2f1275a88ff
Hoang Dinh <nguyendohoangdinh@gmail.com>
2017-07-17 11:37:25 +0700

    second commit

--- Sweetapp-mobile/js/jquery.js
+++ Sweetapp-mobile/js/jquery.js
@@ -11,3 +11,27 @@ $(document).ready(function(){
     return false;
   });
 });
+var Validate = {
+  init: function(){
+    this.validateEmail();
+  },
+  validateEmail: function(){
+    $("#infomation-member").on("click", function(e){
+      e.preventDefault();
+      var erro = 0;
+      var numCode1 = $('input[class="num-code1"]').val()
+      var numCode2 = $('input[class="num-code2"]').val()
+      var regexNumric = new RegExp(/^\+?[0-9(),.-]+$/);
+      if (!regexNumric.test(numCode1) || !regexNumric.test(numCode2)) {
+        $(".alert-code").show();
+        ++erro;
+      } else {
+        $(".alert-code").hide();
+      }
+      if(!erro > 0){
+        $('section').submit();
+        alert('Nhap thong tin chinh xac!!!!');
+      }
+    });
+  },
+};
